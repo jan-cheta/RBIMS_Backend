ae60a5eec98a50b05dc6fd7f44016eba81e41d00
Jan Ryan Ancheta <ancheta03janryan@gmail.com>
2024-05-03 01:55:49 +0800

    newrecord class

---
+++ NewRecord.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RBIMS_Backend
+{
+    public class NewRecord
+    {
+        HouseholdCRUD crudHousehold = new HouseholdCRUD();
+        InhabitantCRUD  crudInhabitant = new InhabitantCRUD();
+        public bool validateNewHouseHold(string householdAddress){
+            List<Household> householdList = crudHousehold.readHousehold();
+
+            foreach(Household household in householdList){
+                if(household.HouseholdAddress.Equals(householdAddress)){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool validateNewInhabitant(string first_name, string last_name, string middle_name, string suffix){
+            List<Inhabitant> inhabitantList= crudInhabitant.readInhabitant();
+            
+            foreach(Inhabitant inhabitant in inhabitantList){
+                if(
+                    inhabitant.FirstName.Equals(first_name)&
+                    inhabitant.LastName.Equals(last_name)&
+                    inhabitant.MiddleName.Equals(middle_name)&
+                    inhabitant.Suffix.Equals(suffix)){
+                        return false;
+                    }
+            }
+            return true;
+        }
+    }
+}
--- obj/Debug/net7.0/RBIMS_Backend.AssemblyInfo.cs
+++ obj/Debug/net7.0/RBIMS_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RBIMS_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33202bb8c4f3308584caccfc57e07a736c53a623")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0631bb504077a2429cfd36aa726ef3fff28def")]
 [assembly: System.Reflection.AssemblyProductAttribute("RBIMS_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RBIMS_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
